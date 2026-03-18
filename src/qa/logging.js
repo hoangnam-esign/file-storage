1281cba5e487f93286b51f4ad6193ad51b71314c
HoangNam <dinhhoangnam998@gmail.com>
2026-03-18 17:17:43 +0700

    staging: fix: not required log_db_conn_string

--- src/qa/logging.js
+++ src/qa/logging.js
@@ -8,8 +8,12 @@ const logConfig = {
   TZ: appEnv.TIME_ZONE,
   loggerOptions: {
     consoleConfig: { mode: appEnv.ENV },
-    mongoConfig: { connectionString: appEnv.LOG_DB_CONNECTION_STRING },
+    // mongoConfig: { connectionString: appEnv.LOG_DB_CONNECTION_STRING },
   },
 };
+
+if (appEnv.LOG_DB_CONNECTION_STRING) {
+  logConfig.loggerOptions.mongoConfig = { connectionString: appEnv.LOG_DB_CONNECTION_STRING };
+}
 
 module.exports = { logConfig };
