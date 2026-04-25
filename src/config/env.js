9202606678438b4e1b4c6002a25ceb563ac87d62
HoangNam <dinhhoangnam998@gmail.com>
2026-04-25 22:17:35 +0700

    update log lib: fix double json stringify

--- src/config/env.js
+++ src/config/env.js
@@ -15,7 +15,7 @@ const appEnvSchema = z.object({
   MONGODB_CONNECT_STRING: z.string(),
   JWT_SECRET: z.string(),
   FILE_SERVER_ENDPOINT: z.string(),
-  ENV: z.enum(["prod", "dev"]).default("dev"),
+  ENV: z.enum(["prod", "dev"]).default("prod"),
 });
 
 const appEnv = appEnvSchema.parse(process.env);
